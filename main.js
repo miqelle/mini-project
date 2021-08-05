3e81e657faf06c836efe821483046a2a3c6678d1
miqelle <miqellec3@gmail.com>
2021-08-05 14:04:38 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/* let button = document.querySelector("button");
+button.addEventListener('click', addReflection); */
+
+//function to ask to add reflections
+const reflectionJournal = function reflections() {
+    let enterDate = prompt("Enter today's date.");
+}
+
+/* reflectionJournal() */
